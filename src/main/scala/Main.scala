2f5f520262366988ea8c2dacc0d88fee5f1d7704
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2023-10-15 11:51:55 +0530

    core added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,21 +18,67 @@ object ZIOHTTP extends ZIOAppDefault {
 
   val app: Http[Any, Nothing, Request, Response] = Http.collect[Request] {
     case Method.GET -> !! / "owls" => Response.text("Hoot!")
-  }
+  }@@ Middleware.csrfGenerate()
 
   val zApp: UHttpApp = Http.collectZIO[Request] {
     case Method.POST -> !! / "owls" => //ZIO.succeed(Response.text("Hoot!"))
       Random.nextIntBetween(3,5).map(n => Response.text("Hello"*n + ", owls" ))
-  }
+  }@@ Middleware.csrfValidate()
 
   val combined = app ++ zApp 
   //val combined = app <> zApp //if first fails, try second
 
+  //middleware 
+  //val wrapped = combined @@ middleware.debug
+  //request -> wrapped -> combined -> response
+  val Ourmiddleware = combined @@ Verbose.log
+
   val httpProgram = for {
     _ <- Console.printLine(s"Server is starting on port ${port}")
-    _ <- Server.start(port, combined) //internally calls zio.never
+    _ <- Server.start(port, Ourmiddleware) //internally calls zio.never
   } yield ()
 
   override def run = httpProgram
-  
+
+  object Verbose {
+    def log[R, E >: Throwable]
+        : Middleware[R, E, Request, Response, Request, Response] =
+      new Middleware[R, E, Request, Response, Request, Response] {
+
+        override def apply[R1 <: R, E1 >: E](
+            http: Http[R1, E1, Request, Response]
+        ): Http[R1, E1, Request, Response] =
+          http
+            .contramapZIO[R1, E1, Request] { r =>
+              for {
+                _ <- Console.printLine(s"> ${r.method} ${r.path} ${r.version}")
+                _ <- ZIO.foreach(r.headers.toList) { h =>
+                        Console.printLine(s"> ${h._1}: ${h._2}")
+                      }
+              } yield r
+            }
+            .mapZIO[R1, E1, Response] { r =>
+              for {
+                _ <- Console.printLine(s"< ${r.status}")
+                _ <- ZIO.foreach(r.headers.toList) { h =>
+                        Console.printLine(s"< ${h._1}: ${h._2}")
+                      }
+              } yield r
+            }
+          }
+      }
+    object Cors {
+      final case class CorsConfig(
+        anyOrigin: Boolean = true,
+        anyMethod: Boolean = true,
+        allowCredentials: Boolean = true,
+        allowedOrigins: String => Boolean = _ => false,
+        allowedMethods: Option[Set[Method]] = None,
+        allowedHeaders: Option[Set[String]] = Some(
+          Set(HttpHeaderNames.CONTENT_TYPE.toString, HttpHeaderNames.AUTHORIZATION.toString, "*"),
+        ),
+        exposedHeaders: Option[Set[String]] = Some(Set("*")),
+      )
+    }
+
 }
